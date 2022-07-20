53d0ce9a1f8fb319cf4afa038aff2aee58eea073
jameer1 <104611941+jameer1@users.noreply.github.com>
2022-07-20 16:08:18 +0530

    Update pot.constants.js

--- pot/web/pot-web/app/scripts/common/constants/pot.constants.js
+++ pot/web/pot-web/app/scripts/common/constants/pot.constants.js
@@ -2,7 +2,7 @@
 
 // this value will not be available during npm start
 // uncomment below line for npm start
-var ORIGIN_URL = 'http://localhost:8080';
+// var ORIGIN_URL = 'http://localhost:8080';
 
 app.constant('POTURL', {
 	// WildFly Server
